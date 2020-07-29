5f82ccf73bce781094084e4cbf0d9e8383f0b7ce
Arif Glory <arifglory46@gmail.com>
2020-07-29 15:23:06 +0700

    working on pesanan

---
+++ app/src/main/java/com/tapisdev/cateringtenda/model/Pesanan.kt
@@ -0,0 +1,15 @@
+package com.tapisdev.cateringtenda.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Parcelize
+data class Pesanan(
+    var pesananId: String? = "",
+    var idAdmin: String? = "",
+    var idUser: String? = "",
+    var alamat: String? = "",
+    var tanggalPesan: String? = ""
+) : Parcelable, java.io.Serializable
