012eda67542e7487782fb18afa8d2dd1d935b774
Arif Glory <arifglory46@gmail.com>
2020-08-05 15:20:24 +0700

    working on konfirmasi pesan

---
+++ app/src/main/java/com/tapisdev/cateringtenda/activity/pengguna/KonfirmasiPesanActivity.kt
@@ -0,0 +1,14 @@
+package com.tapisdev.cateringtenda.activity.pengguna
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tapisdev.cateringtenda.R
+import com.tapisdev.cateringtenda.base.BaseActivity
+
+class KonfirmasiPesanActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_konfirmasi_pesan)
+    }
+}
