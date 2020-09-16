7792c9fabd303e115c6e6e490af18b2bf8f603cf
Arif Glory <arifglory46@gmail.com>
2020-09-16 14:58:29 +0700

    add data tenda di beranda

--- app/src/main/java/com/tapisdev/cateringtenda/fragment/AdminBerandaFragment.kt
+++ app/src/main/java/com/tapisdev/cateringtenda/fragment/AdminBerandaFragment.kt
@@ -15,23 +15,30 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.tapisdev.cateringtenda.R
 import com.tapisdev.cateringtenda.activity.admin.AddCateringActivity
 import com.tapisdev.cateringtenda.adapter.AdapterCatering
+import com.tapisdev.cateringtenda.adapter.AdapterTenda
 import com.tapisdev.cateringtenda.base.BaseFragment
 import com.tapisdev.cateringtenda.model.Catering
+import com.tapisdev.cateringtenda.model.Tenda
 import com.tapisdev.cateringtenda.model.UserPreference
 import kotlinx.android.synthetic.main.fragment_admin_tenda.*
 
 class AdminBerandaFragment : BaseFragment() {
 
     lateinit var rvCatering: RecyclerView
-    lateinit var fab : FloatingActionButton
     var TAG_GET_CATERING = "getCatering"
     lateinit var adapter:AdapterCatering
-
     var listCatering = ArrayList<Catering>()
+
+    lateinit var rvTenda: RecyclerView
+    var TAG_GET_TENDA = "getTenda"
+    lateinit var adapterTenda: AdapterTenda
+    var listTenda = ArrayList<Tenda>()
 
     lateinit var tvNamaPenyedia : TextView
     lateinit var tvAlamat : TextView
     lateinit var tvTelepon : TextView
+    lateinit var tvTitleCatering : TextView
+    lateinit var tvTitleTenda : TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,9 +51,26 @@ class AdminBerandaFragment : BaseFragment() {
         tvNamaPenyedia = root.findViewById(R.id.tvNamaPenyedia)
         tvAlamat = root.findViewById(R.id.tvAlamat)
         tvTelepon = root.findViewById(R.id.tvTelepon)
+        tvTitleCatering = root.findViewById(R.id.tvTitleCatering)
+        tvTitleTenda = root.findViewById(R.id.tvTitleTenda)
+        rvCatering = root.findViewById(R.id.rvCatering)
+        rvTenda = root.findViewById(R.id.rvCatering)
         mUserPref = UserPreference(requireContext())
 
+        adapter = AdapterCatering(listCatering)
+        adapterTenda = AdapterTenda(listTenda)
+
+        rvCatering.setHasFixedSize(true)
+        rvCatering.layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL ,false)
+        rvCatering.adapter = adapter
+
+        rvTenda.setHasFixedSize(true)
+        rvTenda.layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL ,false)
+        rvTenda.adapter = adapterTenda
+
         updateUI()
+        getDataMyCatering()
+        getDataMyTenda()
         return root
     }
 
@@ -54,6 +78,53 @@ class AdminBerandaFragment : BaseFragment() {
         tvNamaPenyedia.setText(mUserPref.getName())
         tvAlamat.setText("Alamat : "+mUserPref.getAlamat())
         tvTelepon.setText("Kontak : "+mUserPref.getPhone())
+    }
+
+    fun getDataMyCatering(){
+        cateringRef.get().addOnSuccessListener { result ->
+            listCatering.clear()
+            //Log.d(TAG_GET_CATERING," datanya "+result.documents)
+            for (document in result){
+                //Log.d(TAG_GET_CATERING, "Datanya : "+document.data)
+                var catering : Catering = document.toObject(Catering::class.java)
+                catering.cateringId = document.id
+                if (catering.idAdmin.equals(auth.currentUser?.uid)){
+                    listCatering.add(catering)
+                }
+            }
+            if (listCatering.size == 0){
+                tvTitleCatering.setText("Belum ada data catering")
+            }
+            adapter.notifyDataSetChanged()
+
+        }.addOnFailureListener { exception ->
+            showErrorMessage("terjadi kesalahan : "+exception.message)
+            Log.d(TAG_GET_CATERING,"err : "+exception.message)
+        }
+    }
+
+    fun getDataMyTenda(){
+        tendaRef.get().addOnSuccessListener { result ->
+            listTenda.clear()
+            //Log.d(TAG_GET_CATERING," datanya "+result.documents)
+            for (document in result){
+                Log.d(TAG_GET_TENDA, "Datanya : "+document.data)
+
+                var tenda : Tenda = document.toObject(Tenda::class.java)
+                tenda.tendaId = document.id
+                if (tenda.idAdmin.equals(auth.currentUser?.uid)){
+                    listTenda.add(tenda)
+                }
+            }
+            if (listTenda.size == 0){
+                tvTitleTenda.setText("Belum ada data alat pesta")
+            }
+            adapterTenda.notifyDataSetChanged()
+
+        }.addOnFailureListener { exception ->
+            showErrorMessage("terjadi kesalahan : "+exception.message)
+            Log.d(TAG_GET_TENDA,"err : "+exception.message)
+        }
     }
 
     companion object {
@@ -68,6 +139,8 @@ class AdminBerandaFragment : BaseFragment() {
 
     override fun onResume() {
         super.onResume()
+        getDataMyCatering()
+        getDataMyTenda()
     }
 
 }
